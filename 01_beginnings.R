b35254c01b0a8132b8bcfcdb44581fc10b9af5b4
eliagenghini <166822529+eliagenghini@users.noreply.github.com>
2024-06-03 16:08:59 +0200

    Update 01_beginnings.R

--- 01_beginnings.R
+++ 01_beginnings.R
@@ -1,21 +1,32 @@
-# First R script (thankyou pietrozoffoli for the help)
+#First R script
 
-# R as a calculation tool ("<-"=create an object)
-a<-6^3
-b<-5*8
+#R as a calculator ("<-"= create an object)
+a <- 6^3
+b <- 5*8
 a+b
 
-# R for arrays and matrix calculation
-
-# this is an array, function "c" is used to group elements into a single object
-
+# R for arrays and matrix calculation ("c"= is used to group elements into a single object)
 flowers<-c(3,6,8,10,15,18)
 insects<-c(10,16,25,42,61,73)
 
-# plotting two arrays (thay have to be the same length)
-# functions: "pch"=type of simbol on the plot; "col"=color; "cex"=point size (character exageration); "xlab and ylab"=names of the x and y axes
-# plot(x,y,..)
+# plot arrays
+plot(flowers, insects)
 
-plot(flowers, insects, pch=19, col='blue', cex=.98, xlab='fiori', ylab='insetti')
+#changing plot parameters:
 
-# I edited this script to see how it works
+#symbols
+plot(flowers, insects, pch=19)
+
+#symbol dimensions
+plot(flowers, insects, pch=19, cex=2)
+plot(flowers, insects, pch=19, cex=.5)
+
+#color
+plot(flowers, insects, pch=19, cex=.5, col="chocolate3")
+
+#name x and y asses
+plot(flowers, insects, pch=19, cex=.5, col="chocolate3", ylab="insetti", xlab="fiori")
+
+
+
+
